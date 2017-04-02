37d863c9122b8c9d360937a9ee2dc5fd37e1a423
sskerr <sarah.s.kerr@gmail.com>
2017-04-02 17:00:34 -0400

    mobile html css, initial js

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 //Add in external javacript file
 $(document).ready(function () { //checks if html is fully loaded
 
+	$("#compute").on("click", function (e) {
+		e.preventDefault();
+		var mango = parseFloat($("#mango").val());
+
+		var total = calculate(mango)
+
+		$("#results").val(total);
+
+	});
+
+
 });
+
+
+function calculate(mango) {
+
+	return ((mango * .53) * ((.75 * 1.20) - .75))
+
+};
