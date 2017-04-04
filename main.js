fcf4c2f57a2138c5820517ddb61fa106842bb871
sskerr <sarah.s.kerr@gmail.com>
2017-04-04 19:36:05 -0400

    answer styling

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 //Add in external javacript file
 $(document).ready(function () { //checks if html is fully loaded
-
+	$("input[type='number']").on("keydown", function (e) {
+		if (e.keyCode === 190) {
+			e.preventDefault();
+		}
+	});
 	$("#compute").on("click", function (e) {
 		e.preventDefault();
 		var mango = parseFloat($("#mango").val());
@@ -15,9 +19,30 @@ $(document).ready(function () { //checks if html is fully loaded
 		var tequila = parseFloat($("#tequila").val());
 
 
-		var total = calculate(mango, avocado, tomato, banana, tangerine, strawberry, lime, pineapple, beer, tequila)
+		var total = calculate(mango, avocado, tomato, banana, tangerine, strawberry, lime, pineapple, beer, tequila);
+		//	var wall = wall(wall);
 
-		$("#results").val(total);
+		// var total = 0;
+		//
+		// $("input[type='number']").each(function () {
+		// 	var fruit = $(this);
+		// 	var storePrice = parseFloat(fruit.data("storePrice"));
+		// 	var likelihood = parseFloat(fruit.data("likelihood"));
+		// 	var qty = parseInt(fruit.val());
+		//
+		// 	var calc = qty * ((storePrice * 1.2) - storePrice);
+		//
+		// 	total = total + calc;
+		// });
+
+		$("#final-answer").addClass("active");
+		$("#final-answer2").addClass("active");
+		$("#wall_answer").addClass("active");
+		$("#wall_answer2").addClass("active");
+		showIt(total);
+		//	wall(total);
+		//	showWall(wall);
+
 
 	});
 
@@ -29,3 +54,15 @@ function calculate(mango, avocado, tomato, banana, tangerine, strawberry, lime, 
 
 	return (((mango * .53) * ((1.75 * 1.20) - 1.75)) + ((avocado * .59) * ((1.5 * 1.20) - 1.5)) + ((tomato * .41) * ((.5 * 1.20) - .5)) + ((banana * .03) * ((.20 * 1.2) - .20)) + ((tangerine * .04) * ((.1 * 1.2) - .1)) + ((strawberry * .08) * ((2.5 * 1.2) - 2.5)) + ((lime * .95) * ((.2 * 1.2) - .2)) + ((tequila * .99) + ((25 * 1.2) - 25))) * 48
 };
+
+function showIt(total) {
+	$("#final-results").text(total.toFixed(2));
+};
+
+//function wall(total) {
+//return (total / 99.68);
+//};
+
+//function showWall(wall) {
+//("#wall").text(wall);
+//}
